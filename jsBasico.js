44cac97acf61e1dfd41faf9996689745a6e77096
Christian Peralta <christianperalta22@gmail.com>
2022-11-29 16:34:45 -0300

    first assignment submission

---
+++ jsBasico.js
@@ -0,0 +1,447 @@
+// 1)
+//Passed:You should create a // style comment that contains at least five letters.
+// Passed:You should create a /* */ style comment that contains at least five letters:
+
+// Hello World
+
+/* coment in
+block*/
+//------------------------------------------------------------------
+
+//2) Passed:You should declare myName with the var keyword, ending with a semicolon
+var myName;
+//------------------------------------------------------------------
+
+//3) Passed:You should not change code above the specified comment.
+//Passed:a should have a value of 7.
+
+// Setup
+var a;
+
+// Only change code below this line
+a=7;
+//------------------------------------------------------------------
+
+// 4) Passed:You should not change code above the specified comment.
+// Passed:b should have a value of 7.
+// Passed:a should be assigned to b with =.
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b=a;
+//------------------------------------------------------------------
+
+//5) You should initialize a to a value of 9.
+var a=9;
+
+//------------------------------------------------------------------
+
+//6)Waiting:myFirstName should be a string with at least one character in it.
+//Waiting:myLastName should be a string with at least one character in it.
+let myFirstName= "Christian";
+let myLastName= "Peralta";
+
+//------------------------------------------------------------------
+
+//7)a should be defined and have a final value of 6.
+// Passed:b should be defined and have a final value of 15.
+// Passed:c should not contain undefined and should have a final value of the string I am a String!
+// Passed:You should not change code below the specified comment.
+// Only change code below this line
+var a=5;
+var b=10;
+var c= "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+//------------------------------------------------------------------
+
+//8)Waiting:studlyCapVar should be defined and have a value of 10.
+// Waiting:properCamelCase should be defined and have a value of the string A String.
+// Waiting:titleCaseOver should be defined and have a value of 9000.
+// Waiting:studlyCapVar should use camelCase in both declaration and assignment sections.
+// Waiting:properCamelCase should use camelCase in both declaration and assignment sections.
+// Waiting:titleCaseOver should use camelCase in both declaration and
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//------------------------------------------------------------------
+
+//9)Passed:var should not exist in the code.
+// catName should be the string Oliver.
+// atSound should be the string Meow!
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//------------------------------------------------------------------
+
+//10) var should not exist in your code.
+// Passed:You should change fCC to all uppercase.
+// Passed:FCC should be a constant variable declared with const.
+// Passed:The string assigned to FCC should not be changed.
+// Passed:fact should be declared with let.
+// Passed:console.log should be changed to print the FCC and fact variables.
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+//------------------------------------------------------------------
+
+//11) Passed:sum should equal 20.
+// Passed:You should use the + operator.
+const sum = 10 + 10;
+
+//------------------------------------------------------------------
+
+
+//12) Passed:The variable difference should be equal to 12.
+// Passed:You should only subtract one number from 45.
+
+const difference = 45 - 33;
+
+//------------------------------------------------------------------
+
+//13) Passed:The variable product should be equal to 80.
+// Passed:You should use the * operator.
+
+const product = 8 * 10;
+
+//------------------------------------------------------------------
+
+//14) Passed:The variable quotient should be equal to 2.
+// Passed:You should use the / operator.
+const quotient = 66 / 33;
+//------------------------------------------------------------------
+
+//15) Passed:myVar should equal 88.
+// Passed:You should not use the assignment operator.
+// Passed:You should use the ++ operator.
+// Passed:You should not change code above the specified comment.
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//------------------------------------------------------------------
+
+//16)myVar should equal 10.
+// Passed:myVar = myVar - 1; should be changed.
+// Passed:You should not assign myVar with 10.
+// Passed:You should use the -- operator on myVar.
+// Passed:You should not change code above the specified comment.
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//------------------------------------------------------------------
+
+//17Passed:myDecimal should be a number.
+//Passed:myDecimal should have a decimal point
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal= ourDecimal;
+
+//------------------------------------------------------------------
+
+//18) Passed:The variable product should equal 5.0.
+//Passed:You should use the * operator
+const product = 2.0 * 2.5;
+//------------------------------------------------------------------
+
+//19) Passed:The variable quotient should equal 2.2
+// Passed:You should use the / operator to divide 4.4 by 2
+// Passed:The quotient variable should only be assigned once
+
+const quotient = 4.4 / 2.0; // Change this line
+//------------------------------------------------------------------
+
+//20)Passed:The variable remainder should be initialized
+// Passed:The value of remainder should be 2
+// Passed:You should use the % operator
+
+const remainder = 11 % 3;
+//------------------------------------------------------------------
+
+//21) a should equal 15.
+// Passed:b should equal 26.
+// Passed:c should equal 19.
+// Passed:You should use the += operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9 ;
+c += 7;
+//------------------------------------------------------------------
+
+//22)Tests
+// Passed:a should equal 5.
+// Passed:b should equal -6.
+// Passed:c should equal 2.
+// Passed:You should use the -= operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=1;
+//------------------------------------------------------------------
+
+//23) Tests
+// Passed:a should equal 25.
+// Passed:b should equal 36.
+// Passed:c should equal 46.
+// Passed:You should use the *= operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+//------------------------------------------------------------------
+
+//24) Tests
+// Passed:a should equal 4.
+// Passed:b should equal 27.
+// Passed:c should equal 3.
+// Passed:You should use the /= operator for each variable.
+// Passed:You should not modify the code above the specified comment.
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+//------------------------------------------------------------------
+
+//25)Passed:You should use two double quotes (") and four escaped double quotes (\").
+// Passed:Variable myStr should contain the string: I am a "double quoted" string inside "double quotes".
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+//------------------------------------------------------------------
+
+
+
+//26)Passed:You should remove all the backslashes (\).
+// Passed:You should have two single quotes ' and four double quotes ".
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//------------------------------------------------------------------
+
+//27) Passed:myStr should not contain any spaces
+// Passed:myStr should contain the strings FirstLine, SecondLine and ThirdLine (remember case sensitivity)
+// Passed:FirstLine should be followed by the newline character \n
+// Passed:myStr should contain a tab character \t which follows a newline character
+// Passed:SecondLine should be preceded by the backslash character \
+// Passed:There should be a newline character between SecondLine and ThirdLine
+// Passed:myStr should only contain characters shown in the instructions
+
+const myStr= "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+//------------------------------------------------------------------
+//28) Passed:myStr should have a single space character between the two strings.
+// Passed:myStr should have a value of the string This is the start. This is the end.
+// Passed:You should use the + operator to build myStr.
+// Passed:myStr should be created using the const keyword.
+// Passed:You should assign the result to the myStr variable.
+
+const myStr = "This is the start. " + "This is the end."; // Change this line
+//------------------------------------------------------------------
+
+//29) Passed:myStr should have a single space character between the two strings.
+// Passed:myStr should have a value of the string This is the first sentence. This is the second sentence.
+// Passed:You should use the += operator to build myStr.
+let myStr="This is the first sentence.";
+myStr+= " This is the second sentence.";
+//------------------------------------------------------------------
+
+//30)Passed:myName should be set to a string at least 3 characters long.
+// Passed:You should use two + operators to build myStr with myName inside it.
+// Only change code below this line
+const myName = "Christian";
+const myStr ="My name:"+ myName + "Hello world";
+//------------------------------------------------------------------
+
+//31)Tests
+// Passed:someAdjective should be set to a string at least 3 characters long.
+// Passed:You should append someAdjective to myStr using the += operator.
+const someAdjective = "awesome!";
+let myStr = "Learning to code is ";
+myStr+= someAdjective;
+//------------------------------------------------------------------
+
+//32)Passed:You should not change the variable declarations in the // Setup section.
+// Passed:lastNameLength should be equal to eight.
+// Passed:You should be getting the length of lastName by using .length like this: lastName.length.
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+//------------------------------------------------------------------
+
+//33) Tests
+// Passed:The firstLetterOfLastName variable should have the value of L.
+// Passed:You should use bracket notation.
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+//------------------------------------------------------------------
+
+//34)Tests
+// Passed:myStr should have a value of the string Hello World.
+// Passed:You should not change the code above the specified comment.
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+//------------------------------------------------------------------
+
+//35)Tests
+// Passed:The thirdLetterOfLastName variable should have the value of v.
+// Passed:You should use bracket notation.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+//------------------------------------------------------------------
+
+
+
+//36)Tests
+// Passed:lastLetterOfLastName should be the letter e.
+// Passed:You should use .length to get the last letter.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+//------------------------------------------------------------------
+
+//37) Tests
+// Passed:secondToLastLetterOfLastName should be the letter c.
+// Passed:You should use .length to get the second last letter.
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+//------------------------------------------------------------------
+// //38) Passed:wordBlanks should be a string.
+// Passed:You should not change the values assigned to myNoun, myVerb, myAdjective or myAdverb.
+// Passed:You should not directly use the values dog, ran, big, or quickly to create wordBlanks.
+// Passed:wordBlanks should contain all of the words assigned to the variables myNoun, myVerb,
+//  myAdjective and myAdverb separated by non-word characters (and any additional words of your choice).
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My" +" "+ myAdjective+" " + myNoun+" "+ myVerb+" "+myVerb+" " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+//------------------------------------------------------------------
+
+//39) Tests
+// Passed:myArray should be an array.
+// Passed:The first item in myArray should be a string.
+// Passed:The second item in myArray should be a number.
+
+// Only change code below this line
+const myArray = ["el numero", 2];
+//------------------------------------------------------------------
+
+//40)Tests
+// Passed:myArray should have at least one array nested within another array.
+// Only change code below this line
+const myArray = [["Polonia", 4],["Argentina", 3],["Arabia Saudita", 3],["Mexico", 1]];
+
+//------------------------------------------------------------------
+
+//41)Tests
+// Passed:The variable myData should equal the first value of myArray.
+// Passed:The data in variable myArray should be accessed using bracket notation.
+const myArray = [50, 60, 70];
+const myData = myArray[0];
+
+//------------------------------------------------------------------
+
+//42)Tests
+// Passed:myArray should now be [45, 64, 99].
+// Passed:You should be using correct index to modify the value in myArray.
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0]=45;
+//------------------------------------------------------------------
+
+//43)Tests
+// Passed:myData should be equal to 8.
+// Passed:You should be using bracket notation to read the correct value from myArray.
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
+//------------------------------------------------------------------
+
+//44) Tests
+// Passed:myArray should now equal [["John", 23], ["cat", 2], ["dog", 3]].
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog",3]);
+//------------------------------------------------------------------
+
+//45)Tests
+// Passed:myArray should only contain [["John", 23]].
+// Passed:You should use pop() on myArray.
+// Passed:removedFromMyArray should only contain ["cat", 2].
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+
+var removedFromMyArray=myArray.pop();;
+
+//------------------------------------------------------------------
